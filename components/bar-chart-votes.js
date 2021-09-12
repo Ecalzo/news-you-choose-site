c649f49962b63536fb09234110e5c4b178971029
ecalzo <evancalz@me.com>
2021-09-12 11:31:48 -0400

    make bar chart slightly more visible

--- components/bar-chart-votes.js
+++ components/bar-chart-votes.js
@@ -25,9 +25,9 @@ export default function BarChartVotes({ votes }) {
         label: "# of votes",
         data: votes.map((vote) => vote.counts),
         backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 99, 132, 0.4)",
+          "rgba(255, 206, 86, 0.4)",
+          "rgba(54, 162, 235, 0.4)",
         ],
         borderColor: [
           "rgba(255, 99, 132, 1)",
