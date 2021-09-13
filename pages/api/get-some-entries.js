ddc4e78e9f0ebd7315b69113942c9b35c4337ab8
ecalzo <evancalz@me.com>
2021-09-13 19:55:40 -0400

    dedupe database results and get more neutral articles

--- pages/api/get-some-entries.js
+++ pages/api/get-some-entries.js
@@ -18,10 +18,10 @@ const handler = async (req, res) => {
     SELECT id, title, content, src, url, sentiment, score, user_approve, user_disapprove, date, image_url
     FROM news
     WHERE sentiment = ?
-    AND date between DATE_ADD(?, INTERVAL -10 DAY) and ?
+    AND date between DATE_ADD(?, INTERVAL -20 DAY) and ?
     AND score > 60
     ORDER BY date DESC
-    LIMIT 20
+    LIMIT 40
     `;
     } else {
       sqlQuery = `
@@ -31,12 +31,15 @@ const handler = async (req, res) => {
       AND date between DATE_ADD(?, INTERVAL -10 DAY) and ?
       AND score > 95
       ORDER BY date DESC
-      LIMIT 20
+      LIMIT 40
       `;
     }
     const results = await query(sqlQuery, [sentiment, date, date]);
-
-    return res.json(results);
+    const dedupedResults = results.filter(
+      (arr, index, self) =>
+        index === self.findIndex((t) => t.title == arr.title)
+    );
+    return res.json(dedupedResults);
   } catch (e) {
     res.status(500).json({ message: e.message });
   }
