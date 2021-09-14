cbe347d0727c826bd3811694fc5c047c3c7b9579
ecalzo <evancalz@me.com>
2021-09-13 22:20:03 -0400

    rm console.log statements

--- components/bar-chart-votes.js
+++ components/bar-chart-votes.js
@@ -3,7 +3,6 @@ import React from "react";
 import { Bar } from "react-chartjs-2";
 
 export default function BarChartVotes({ votes }) {
-  console.log(votes);
   const options = {
     title: {
       display: true,
